a37faf8e3086e863d305a88870658ccd391fb8ba
Alexander Karlsson <alexander.jr.karlsson@gmail.com>
2022-01-03 13:48:48 +0100

    optimized code

--- code.js
+++ code.js
@@ -4,41 +4,28 @@ const Root = document.querySelector(":root");
 const SeedInput = document.getElementById("Seed");
 const ErrorCode = document.getElementById("Error-code");
 
-randomizeBTN.addEventListener("click", Switch);
+randomizeBTN.addEventListener("click", Creator);
 
-const HexChart = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
-const Invalids = ["",null,undefined];
+const HexChart = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]; //Used for generating hex colors
+const Allowed = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","X","#"];
+const Invalids = ["",null,undefined,NaN];
 
-let checker = (array, item) => array.every(y => {![item].includes(y)});
+let checker = (array, item) => !item.every(y => !array.includes(y)); //Checks if an item is not in an array
+let verifyer = (array1, array2) => array2.every(y => array1.includes(y)); //Checks if every element is in array2 is in array1
 
-// SeedInput.value != "" && SeedInput.value != null && SeedInput.value != undefined
-function Switch() {
-    let temp = SeedInput.value.split("");
-    if (!checker(Invalids,temp)) {
-        RandomSeed();
-    }
-    else {
-        Random();
-    }
-}
-
-function Random() {
-    let newColor = "#";
-    for (let x = 0; x < 6; x++){
-        newColor = newColor + String(HexChart[Math.floor(Math.random()*HexChart.length)]);
-    }
-    Root.style.setProperty('--Color', newColor);
-    Display.innerHTML = newColor;
-}
-
-function RandomSeed() {
+//The Creator function generates hex colors
+function Creator() {
     let newColor = SeedInput.value;
     newColor = newColor.replaceAll(" ","");
     newColor = newColor.toUpperCase();
     if (!newColor.includes("#")) {
         newColor = "#" + newColor;
     }
-    while (newColor.includes("X") == true){
+    if (!verifyer(Allowed, newColor.split(""))) {
+        ErrorCode.innerHTML = "Invalid Character";
+        return;
+    }
+    while (newColor.includes("X") === true){
         newColor = newColor.replace("X",String(HexChart[Math.floor(Math.random()*HexChart.length)]));
     }
     while (newColor.length < 7){
@@ -46,6 +33,7 @@ function RandomSeed() {
     }
     Root.style.setProperty('--Color', newColor);
     Display.innerHTML = newColor;
+    ErrorCode.innerHTML = "";
 }
 
-Random();
+Creator();
